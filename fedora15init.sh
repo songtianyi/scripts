3398c4a70d287b6996ec7c27f02dec944eb38013
songtianyi <songtianyi630@163.com>
2013-06-24 10:27:46 +0800

    get back spice deps log

--- fedora15init.sh
+++ fedora15init.sh
@@ -17,34 +17,38 @@ exit 1
 sudo yum -y install yum-fastestmirror
 sudo yum -y update
 
-#install dstat
+#install some useful tools
 sudo yum -y install dstat
-
-#install vim
 sudo yum -y install vim
-
-#install adobe flash
-
-#install chromium
-
-#install word processor
+sudo yum -y install wget
 sudo yum -y install abiword
-
-#network config tools
 sudo yum -y install bridge-utils
 sudo yum -y install tunctl
+sudo yum -y install git
+sudo yum -y install httpd
+sudo yum -y install telnet
+
+#load some kernel modules
 sudo modprobe tun
 
-#install git tool
-sudo yum -y install git
+#install chromium
+#install adobe flash
 
 #install gcc compiler
 sudo yum -y install gcc gcc-c++
 
 
-#install VNC
-sudo yum -y install vnc
+##################spice#########################
 
+sudo yum -y install glib2-devel
+sudo yum -y install zlib-devel
+sudo yum -y install pixman-devel
+sudo yum -y install autoconf
+sudo yum -y install celt051-devel
+sudo yum -y install pyparsing
+sudo yum -y install alsa-lib-devel
+sudo yum -y install openssl-devel
+sudo yum -y install libcacard libcacard-devel 
 
 ###################KVM##########################
 
@@ -54,14 +58,17 @@ sudo modprobe kvm_intel
 
 #install libvirt and virt tools
 sudo yum -y install libvirt libvirt-devel
-sudo yum -y install virt-top
+sudo yum -y install virt-topi
+
 ###################qemu#########################
+
+#install VNC
+sudo yum -y install vnc
 
 #when you meet Failed to open /dev/dsp oss: No such file or Directory problem
 sudo modprobe snd_pcm_oss
 
 
-##################spice#########################
 
 
 
