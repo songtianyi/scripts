2ee63beeeabe6820b7c41d79e3b3cc31c37f556a
songtianyi <songtianyi630@163.com>
2013-06-26 09:16:58 +0800

    sort out some commands

--- fedora15init.sh
+++ fedora15init.sh
@@ -22,11 +22,13 @@ sudo yum -y install dstat
 sudo yum -y install vim
 sudo yum -y install wget
 sudo yum -y install abiword
+sudo yum -y install dia
 sudo yum -y install bridge-utils
 sudo yum -y install tunctl
 sudo yum -y install git
 sudo yum -y install httpd
 sudo yum -y install telnet
+sudo yum -y install davfs2
 
 #install chromium
 #install adobe flash
@@ -34,11 +36,7 @@ sudo yum -y install telnet
 #install gcc compiler
 sudo yum -y install gcc gcc-c++
 
-############for spice,kvm or qemu###############
-sudo modprobe tun
-sudo modprobe vhost-net
-sudo modprobe virtio_net
-sudo modprobe virtio_blk
+
 ##################spice#########################
 
 sudo yum -y install glib2-devel
@@ -50,32 +48,40 @@ sudo yum -y install pyparsing
 sudo yum -y install alsa-lib-devel
 sudo yum -y install openssl-devel
 sudo yum -y install libcacard libcacard-devel 
+sudo yum -y install cairo-devel cairo
 
-###################KVM##########################
 
-#load kvm module,always needed afther you restart your system
-sudo modprobe kvm
-sudo modprobe kvm_intel
-
-#install libvirt and virt tools
-sudo yum -y install libvirt libvirt-devel
-sudo yum -y install virt-topi
+#if you wanna enable gui,cegui is needed
+#when you compile cegui, freetype2 and libpcre must be installed
+sudo yum install -y freetype-devel
+sudo yum install -y pcre-devel
 
 ###################qemu#########################
 
 #install VNC
 sudo yum -y install vnc
 
-#when you meet Failed to open /dev/dsp oss: No such file or Directory problem
-sudo modprobe snd_pcm_oss
-
-
 #install libaio-devel to enable linux-AIO
 sudo yum -y install libaio-devel libaio
 #install SDL-devel to enable SDL
 sudo yum -y install SDL-devel
+
+############for spice,kvm or qemu###############
+sudo modprobe tun
+sudo modprobe vhost-net
+sudo modprobe virtio_net
+sudo modprobe virtio_blk
+
+#load kvm module,always needed afther you restart your system
+sudo modprobe kvm
+sudo modprobe kvm_intel
+
+#when you meet Failed to open /dev/dsp oss: No such file or Directory problem
+sudo modprobe snd_pcm_oss
+
+#install libvirt and virt tools
+sudo yum -y install libvirt libvirt-devel
+sudo yum -y install virt-top
+
 ################################################
-
-
-
 
