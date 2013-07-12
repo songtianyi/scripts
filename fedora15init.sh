01336b62e32db627c9bfbafe9437b53517be880b
usr1 <usr1@cloudstack.(none)>
2013-07-12 17:19:39 +0800

    i'm back

--- fedora15init.sh
+++ fedora15init.sh
@@ -8,9 +8,17 @@ if [ $kernel = "2.6.38.6-26.rc1.fc15.x86_64" ];then
 	echo "OK"
 else
 	echo "check your kernel version,you can delete the checking code,but i can't promise it will work on your linux system"
+	exit 1
 fi
-exit 1
 
+
+
+
+#essential tools
+sudo yum -y install vim
+sudo yum -y install wget
+sudo yum -y install make
+sudo yum -y install gcc gcc-c++
 
 
 
@@ -26,7 +34,6 @@ sudo yum -y install yum-fastestmirror
 sudo yum -y install httpd
 sudo yum -y install telnet
 sudo yum -y install davfs2
-sudo yum -y install emacs
 sudo yum -y install git
 sudo yum -y install svn
 sudo yum -y install compress
@@ -35,15 +42,13 @@ cd /etc/yum.repos.d/
 sudo wget http://repos.fedorapeople.org/repos/spot/chromium/fedora-chromium-stable.repo
 sudo yum -y install chromium
 #install adobe flash
+sudo yum install http://linuxdownload.adobe.com/adobe-release/adobe-release-x86_64-1.0-1.noarch.rpm -y
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-adobe-linux
+sudo yum install flash-plugin -y
 #network tools 
 sudo yum -y install dstat
-sudo yum -y install wget
 sudo yum -y install bridge-utils
 sudo yum -y install tunctl
-#essential tools
-sudo yum -y install vim
-sudo yum -y install make
-sudo yum -y install gcc gcc-c++
     
 
 
